3b730a7f4f8ec10ef223599cc4c9cc41c7157b65
mcdonaldduncan <380939@colum.edu>
2021-12-23 18:51:19 -0600

    Learning Project 1
    Basic movement scripts and interaction.

---
+++ LearnUnity1/Assets/Scripts/Bomb.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        Respawn();
+    }
+
+    void Update()
+    {
+        if (transform.position.y < -10)
+        {
+            Respawn();
+        }
+    }
+
+    private void Respawn()
+    {
+        float randomX = UnityEngine.Random.Range(-10, 10);
+        float randomY = UnityEngine.Random.Range(5, 10);
+        transform.position = new Vector3(randomX, randomY);
+        var rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = Vector3.zero;
+    }
+}
---
+++ LearnUnity1/Assets/Scripts/CubeMover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CubeMover : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 1;
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, vertical);
+
+        transform.position += movement * Time.deltaTime * speed;
+        // Multiply by Time.deltaTime to tie movement to time not frame. Checks the time since last call and multiplies by value to keep timing consistent.
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        SceneManager.LoadScene(0);
+    }
+}
