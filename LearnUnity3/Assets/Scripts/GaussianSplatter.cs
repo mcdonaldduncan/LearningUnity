3e6cb7dc681d683ab3765192a89bb67ab36b6f66
mcdonaldduncan <380939@colum.edu>
2022-01-03 23:41:26 -0600

    Update GaussianSplatter.cs

--- LearnUnity3/Assets/Scripts/GaussianSplatter.cs
+++ LearnUnity3/Assets/Scripts/GaussianSplatter.cs
@@ -4,17 +4,10 @@ using UnityEngine;
 
 public class GaussianSplatter : MonoBehaviour
 {
+    [SerializeField]public int splatterCount = 1000;
+
     public List<GameObject> splatters = new List<GameObject>();
-    Vector3 location;
-    
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+   
     void FixedUpdate()
     {
         Splat();
@@ -23,16 +16,22 @@ public class GaussianSplatter : MonoBehaviour
     void Splat()
     {
         
-        if (splatters.Count < 5000)
+        if (splatters.Count < splatterCount)
         {
             GameObject @object = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            location = new Vector3(Random.Range(Random.Range(-30f, 30f), Random.Range(-30f, 30f)), Random.Range(Random.Range(-30f, 30f), Random.Range(-30f, 30f)), 0f);
+            var location = GaussianVector(-30f, 30f);
             @object.transform.position = location;
             splatters.Add(@object);
         }
-        //create a method that takes float args and returns the gaussian dist for the two
+        //create a method that takes float args and returns gaussian distribution
 
         //shade spheres based on how many other colliders are touching
+    }
+
+    Vector3 GaussianVector(float negativeBound, float positiveBound)
+    {
+        var gaussianVector = new Vector3(Random.Range(Random.Range(negativeBound, positiveBound), Random.Range(negativeBound, positiveBound)), Random.Range(Random.Range(negativeBound, positiveBound), Random.Range(negativeBound, positiveBound)), 0f);
+        return gaussianVector;
     }
 
 }
