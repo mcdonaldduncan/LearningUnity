eb1aa1f8e213c205dd083e80d7e4586ea35d1ae6
mcdonaldduncan <380939@colum.edu>
2022-01-19 18:40:17 -0600

    Update Controller.cs

--- ModCube/Assets/ModTheCube/Controller.cs
+++ ModCube/Assets/ModTheCube/Controller.cs
@@ -6,8 +6,7 @@ public class Controller : MonoBehaviour
 {
     [SerializeField] int cubeCount = 100;
     [SerializeField] GameObject cube;
-
-    List<GameObject> cubes = new List<GameObject>();
+    List<GameObject> cubes = new();
     Vector3 windowBounds;
     float x, y;
 
