52157c537892c20a92e5f216cb7e3e85108a473c
mcdonaldduncan <380939@colum.edu>
2022-02-01 13:31:05 -0600

    update

---
+++ LearnUnity4/Assets/Scripts/Test.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    float rand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rand = Random.value;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
